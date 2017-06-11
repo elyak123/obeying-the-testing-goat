8499a56073f97dbe1a768ef8dfba5d5135e3f7e0
Javier Llamas <javier.llamasramirez@gmail.com>
2017-06-11 01:58:01 -0500

    pasan todas las pruebas, falta una, la estamos buscando

--- lists/views.py
+++ lists/views.py
@@ -6,16 +6,20 @@ from lists.models     import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
-    #import pdb; pdb.set_trace()
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'items': items, 'list': list_})
 
 def new_list(request):
     if request.POST:
         list_ = List.objects.create()
         Item.objects.create(text=request.POST['item_text'], list=list_)
-        url = reverse('view_list')
+        list_.save()
+        url = reverse('view_list', args=[list_.id])
         return redirect(url)
-    else:
-        return HttpResponse('bla')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_id}/')
