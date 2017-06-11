089c6d8fb18f222ec667182993333b51e5d621cf
Javier Llamas <javier.llamasramirez@gmail.com>
2017-06-11 07:41:58 -0500

    Se encuentra la prueba faltante y se simplifica algun codigo en views y list.html

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,9 @@ from lists.models             import Item, List
 from django.http              import HttpRequest
 
 class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
