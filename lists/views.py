7dfbd9aa235e32abc16679eb9e68f039025d738b
Javier Llamas <javier.llamasramirez@gmail.com>
2017-08-13 09:47:05 -0500

    change from Item.objects.create to Item()

--- lists/views.py
+++ lists/views.py
@@ -10,13 +10,14 @@ def home_page(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     if request.method == 'POST':
-        item = Item.objects.create(text=request.POST['item_text'], list=list_)
+        item = Item(text=request.POST['item_text'], list=list_)
         try:
             item.full_clean()
         except ValidationError:
             error = "You can't have an empty list item"
-            item.delete()
+            #item.delete()
             return render(request, 'list.html', {'list': list_ ,'error': error})
+        item.save()
         return redirect(list_)
     return render(request, 'list.html', {'list': list_})
 
