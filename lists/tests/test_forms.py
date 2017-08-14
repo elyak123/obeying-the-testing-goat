38e10abcf7ccc6d34b356326deba96e25ff69db1
MacJLV <jllamas@insecami.com.mx>
2017-08-14 18:23:25 -0500

    Full pass FT and UTT

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,17 +1,27 @@
-from django.test import TestCase
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from django.test  import TestCase
+from lists.models import Item, List
+from lists.forms  import ItemForm, EMPTY_ITEM_ERROR
 
 class ItemFormTest(TestCase):
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
-        self.assertIn('paceholder="Enter a to-do item"', form.as_p())
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
 
     def test_validation_form_for_blank_items(self):
-        form = ItemForm(data={'text': ''})
+        list_ = List.objects.create()
+        form  = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['text'],
             [EMPTY_ITEM_ERROR]
         )
         with self.assertRaises(ValueError):
-            form.save()
+            form.save(for_list= list_)
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_    = List.objects.create()
+        form     = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list,list_)
