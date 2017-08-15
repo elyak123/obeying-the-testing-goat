7d851904f9fd221ab6deed61e2b846b3ddcfd898
MacJLV <jllamas@insecami.com.mx>
2017-08-15 18:53:34 -0500

    more tests

--- lists/models.py
+++ lists/models.py
@@ -4,9 +4,16 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 class List(models.Model):
 
-	def get_absolute_url(self):
-		return reverse('view_list', args=[self.id])
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        unique_together = ('list', 'text')
+        ordering = ('id',)
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -48,3 +48,21 @@ class ListAndItemModelsTest(TestCase):
         with self.assertRaises(ValidationError):
             item = Item(list=list_, text='bla')
             item.full_clean()
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item.objects.create(list=list2, text='bla')
+        item.full_clean()
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='item1')
+        item2 = Item.objects.create(list=list1, text='item2')
+        item3 = Item.objects.create(list=list1, text='item3')
+        self.assertEqual(list(Item.objects.all()), [item1, item2, item3])
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
